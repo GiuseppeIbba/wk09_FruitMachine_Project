e8615d73700e8283dcaaa27a47d5c280b8553508
Giuseppe <giuseppe.ibba@hotmail.com>
2018-01-31 22:10:35 +0000

    Created Fruit properties getters

--- app/src/main/java/example/codeclan/com/javafruitmachine/Fruit.java
+++ app/src/main/java/example/codeclan/com/javafruitmachine/Fruit.java
@@ -5,5 +5,22 @@ package example.codeclan.com.javafruitmachine;
  */
 
 class Fruit {
+
+        String name;
+        int value;
+
+        public Fruit(String name, int value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
     
 }
