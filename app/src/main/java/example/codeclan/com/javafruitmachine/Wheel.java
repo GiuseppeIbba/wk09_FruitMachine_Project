216d527305714721cef5fab94f832df4b4c7c98d
Giuseppe <giuseppe.ibba@hotmail.com>
2018-01-31 22:25:12 +0000

    Created getters for Wheel property and getItem for the ArrayList

--- app/src/main/java/example/codeclan/com/javafruitmachine/Wheel.java
+++ app/src/main/java/example/codeclan/com/javafruitmachine/Wheel.java
@@ -1,8 +1,30 @@
 package example.codeclan.com.javafruitmachine;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 31/01/2018.
  */
 
 public class Wheel {
+
+    private String position;
+    private ArrayList<Fruit> fruits;
+    private int currentFruitIndex;
+
+
+    public Wheel(String position, ArrayList<Fruit> fruits) {
+        this.position = position;
+        this.fruits = fruits;
+    }
+
+
+    public String getPosition() {
+        return position;
+    }
+
+    public Fruit getFruit() {
+        return this.fruits.get(0);
+    }
+
 }
