14af40d8d6a9933cab6532c3a679a9603cc272c3
Giuseppe <giuseppe.ibba@hotmail.com>
2018-01-31 22:45:37 +0000

    Created Player.java class and getter tests for Player properties

--- app/src/test/java/example/codeclan/com/javafruitmachine/PlayerTest.java
+++ app/src/test/java/example/codeclan/com/javafruitmachine/PlayerTest.java
@@ -1,9 +1,30 @@
 package example.codeclan.com.javafruitmachine;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 31/01/2018.
  */
 
 public class PlayerTest {
-    
+
+    Player player;
+
+    @Before
+    public void before() {
+        player = new Player("Giuseppe", 50);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Giuseppe", player.getName());
+    }
+
+    @Test
+    public void canGetCredit() {
+        assertEquals(50, player.getCredit());
+    }
 }
