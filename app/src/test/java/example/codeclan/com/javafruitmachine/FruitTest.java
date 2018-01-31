735dd083b4caf601d7f406e2e855c3fa015f817b
Giuseppe <giuseppe.ibba@hotmail.com>
2018-01-31 22:08:50 +0000

    Created Fruit.java amd getter tests for the properties

--- app/src/test/java/example/codeclan/com/javafruitmachine/FruitTest.java
+++ app/src/test/java/example/codeclan/com/javafruitmachine/FruitTest.java
@@ -1,8 +1,29 @@
 package example.codeclan.com.javafruitmachine;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by user on 31/01/2018.
  */
 
 public class FruitTest {
+
+    Fruit fruit;
+
+    @Before
+    public void before() {
+        fruit = new Fruit("Apple", 10);
+    }
+
+    @Test
+    public void canHaveName() {
+        assertEquals("Apple", fruit.getName());
+    }
+
+    @Test
+    public void canHaveValue() {
+        assertEquals(10, fruit.getValue());
+    }
+}
 }
